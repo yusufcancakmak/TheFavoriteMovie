6efd430ed7d22b618fd59a2c1b8a438fc7e7340c
Alıeren <alierendurmus35@gmail.com>
2023-05-26 21:51:56 +0300

    hello world

--- app/src/main/java/com/hera/thefavoritemovie/MainActivity2.kt
+++ app/src/main/java/com/hera/thefavoritemovie/MainActivity2.kt
@@ -3,10 +3,13 @@ package com.hera.thefavoritemovie
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main2)
     }
+
+
+
 
 }
